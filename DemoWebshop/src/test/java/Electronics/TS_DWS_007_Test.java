d9de1c15d53fa84335dc8b2f9b50fe4c8a239489
SnehaOvi <snehamangrulkar1992@gmail.com>
2024-12-24 16:37:19 +0530

    Update TS_DWS_007_Test.java

--- DemoWebshop/src/test/java/Electronics/TS_DWS_007_Test.java
+++ DemoWebshop/src/test/java/Electronics/TS_DWS_007_Test.java
@@ -22,4 +22,5 @@ public class TS_DWS_007_Test extends BaseClass
 		Assert.assertEquals(driver.getTitle(), "Demo Web Shop. Electronics", "Electronics page is not displayed");
 		test.log(Status.PASS, "Electronics page is displayed");
 	}
+	Reporter.log("Hi");
 }
